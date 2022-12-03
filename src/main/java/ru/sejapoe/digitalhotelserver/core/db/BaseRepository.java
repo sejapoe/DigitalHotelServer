33a60f0b28ac0e5975de939c57050eba59f9f040
sejapoe <bekaldiev2015@gmail.com>
2022-12-03 22:45:17 +0300

    fix

--- src/main/java/ru/sejapoe/digitalhotelserver/core/db/BaseRepository.java
+++ src/main/java/ru/sejapoe/digitalhotelserver/core/db/BaseRepository.java
@@ -1,8 +1,10 @@
 package ru.sejapoe.digitalhotelserver.core.db;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.math.BigInteger;
 
+@NoRepositoryBean
 public interface BaseRepository<T> extends MongoRepository<T, BigInteger> {
 }
